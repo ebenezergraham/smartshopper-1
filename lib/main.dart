f975d5ae3de87b52b83f6fc9ff77a041b0a07170
Apondi <kevpreneur@gmail.com>
2018-09-20 01:00:55 +0400

    Login authentication that allows login with Phone, Facebook and Gmail

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,35 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() => runApp(new MyApp());
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:smartshopper/homepage.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = new GoogleSignIn();
+final FacebookLogin _fbLogin = new FacebookLogin();
+
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Firebase Auth Demo',
+      home: new MyHomePage(title: 'Firebase Auth Demo'),
+      routes: <String, WidgetBuilder>{
+        //5
+        '/homepage': (BuildContext context) => new MyHomepage(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -43,67 +38,151 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Future<String> _message = new Future<String>.value('');
+  String verificationId;
+  String testPhoneNumber; //changed
+  final TextEditingController _phoneNoController = new TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  Future<String> _testSignInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final FirebaseUser user = await _auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    Navigator.of(context).pushReplacementNamed('/homepage');
+
+    return 'signInWithGoogle succeeded: $user';
+  }
+
+  Future<String> _loginWithFacebook() async {
+    _fbLogin
+        .logInWithReadPermissions(['email', 'public_profile']).then((result) {
+      switch (result.status) {
+        case FacebookLoginStatus.loggedIn:
+          FirebaseAuth.instance
+              .signInWithFacebook(accessToken: result.accessToken.token)
+              .then((signedInUser) {
+            print('Signed in as ${signedInUser.displayName}');
+            Navigator.of(context).pushReplacementNamed('/homepage');
+          }).catchError((e) {
+            print(e);
+          });
+      }
+    }).catchError((e) {
+      print(e);
     });
+
+    return 'Successfully logged in';
+  }
+
+  Future<void> _testVerifyPhoneNumber() async {
+    final PhoneVerificationCompleted verificationCompleted =
+        (FirebaseUser user) {
+      setState(() {
+        _message =
+            Future<String>.value('signInWithPhoneNumber auto succeeded: $user');
+        Navigator.of(context).pushReplacementNamed('/homepage');
+      });
+    };
+
+    final PhoneVerificationFailed verificationFailed =
+        (AuthException authException) {
+      setState(() {
+        _message = Future<String>.value(
+            'Phone numbber verification failed. Code: ${authException.code}. Message: ${authException.message}');
+      });
+    };
+
+    final PhoneCodeSent codeSent =
+        (String verificationId, [int forceResendingToken]) async {
+      this.verificationId = verificationId;
+    };
+
+    final PhoneCodeAutoRetrievalTimeout codeAutoRetrievalTimeout =
+        (String verificationId) {
+      this.verificationId = verificationId;
+    };
+
+    await _auth.verifyPhoneNumber(
+        phoneNumber: testPhoneNumber,
+        timeout: const Duration(seconds: 5),
+        verificationCompleted: verificationCompleted,
+        verificationFailed: verificationFailed,
+        codeSent: codeSent,
+        codeAutoRetrievalTimeout: codeAutoRetrievalTimeout);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+      body: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              left: 16.0,
+              right: 16.0,
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            child: new TextField(
+              controller: _phoneNoController,
+              decoration: const InputDecoration(
+                hintText: 'Enter phone number',
+              ),
             ),
-          ],
-        ),
+          ),
+          new Container(
+            margin: const EdgeInsets.only(
+              top: 8.0,
+              bottom: 8.0,
+              left: 16.0,
+              right: 16.0,
+            ),
+            child: new RaisedButton(
+              color: Colors.red,
+              onPressed: () {
+                if (_phoneNoController.text != null) {
+                  this.testPhoneNumber = _phoneNoController.text;
+                  _testVerifyPhoneNumber();
+                }
+              },
+              child: new Icon(Icons.arrow_forward),
+            ),
+          ),
+          new MaterialButton(
+              child: const Text('Log In With Google'),
+              onPressed: () {
+                setState(() {
+                  _message = _testSignInWithGoogle();
+                });
+              }),
+          new MaterialButton(
+              child: const Text('Log In With Facebook'),
+              onPressed: () {
+                _message = _loginWithFacebook();
+              }),
+          new FutureBuilder<String>(
+              future: _message,
+              builder: (_, AsyncSnapshot<String> snapshot) {
+                return new Text(snapshot.data ?? '',
+                    style:
+                        const TextStyle(color: Color.fromARGB(255, 0, 155, 0)));
+              }),
+        ],
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
